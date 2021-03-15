fd20803b1b87738a2d648d5336e78931a62a4196
Andrea <e11809650@student.tuwien.ac.at>
2021-03-15 11:24:28 +0100

    [US5] added getSportById so sport.name will be displayed in details

--- frontend/src/app/component/horse-details/horse-details.component.ts
+++ frontend/src/app/component/horse-details/horse-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Horse} from '../../dto/horse';
 import {HorseService} from '../../service/horse.service';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
+import {Sport} from '../../dto/sport';
+import {SportService} from '../../service/sport.service';
 
 @Component({
   selector: 'app-horse-details',
@@ -11,19 +13,30 @@ import {Location} from '@angular/common';
 })
 export class HorseDetailsComponent implements OnInit {
   horse: Horse;
+  sport: Sport;
 
   constructor(private horseService: HorseService,
               private route: ActivatedRoute,
-              private location: Location) { }
-
-  ngOnInit(): void {
-    this.getHorse();
+              private location: Location,
+              private sportService: SportService) {
   }
 
-  getHorse(): void {
+  ngOnInit(): void {
+    this.getHorseAndSport();
+  }
+
+  /**
+   * load the horse with the ID specified in URL
+   */
+  getHorseAndSport(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.horseService.getHorseById(id)
-      .subscribe(horse => this.horse = horse);
+      .subscribe(horse => {
+        this.horse = horse;
+        console.log('horse and sport:');
+        console.log(this.horse);//todo: remove
+        this.getSportById(this.horse.favSportId);
+      });
   }
 
   /**
@@ -32,5 +45,13 @@ export class HorseDetailsComponent implements OnInit {
   goBack() {
     this.horse = null;
     this.location.back();
+  }
+
+  private getSportById(id: number) {
+    this.sportService.getSportById(id).subscribe(
+      (sport: Sport) => {
+        this.sport = sport;
+        console.log(sport); //todo: remove
+      });
   }
 }
