a51ee33bf18bf54c640ed2ae97112bcc9757e81f
Andrea <e11809650@student.tuwien.ac.at>
2021-03-26 18:21:41 +0100

    [US9] added delete and details button to tree

--- frontend/src/app/component/bloodline/bloodline.component.ts
+++ frontend/src/app/component/bloodline/bloodline.component.ts
@@ -3,6 +3,7 @@ import {Horse} from '../../dto/horse';
 import {Location} from '@angular/common';
 import {TreeHorse} from '../../dto/TreeHorse';
 import {HorseService} from '../../service/horse.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -23,7 +24,8 @@ export class BloodlineComponent implements OnInit {
   genNum: number;
 
   constructor(private location: Location,
-              private horseService: HorseService) {
+              private horseService: HorseService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -52,8 +54,44 @@ export class BloodlineComponent implements OnInit {
     if (this.generations === null) {
       return false;
     }
-    return !isNaN(Number(this.generations));
+    if(this.generations.includes('.') || this.generations.includes(',')){
+      return false;
+    }
+    if(isNaN(Number(this.generations))){
+      return false;
+    }
+    if(Number(this.generations)<1){
+      return false;
+    }
+    return true;
+  }
 
+  /**
+   * deletes horse from db and parent-child relationships
+   *
+   * @param id of the horse to be deleted
+   * @param name: name of the horse to be deleted
+   */
+  deleteHorse(id: number, name: string) {
+    this.horseService.deleteHorse(id).subscribe(() => {
+        alert('Deleted horse ' + name + '.');
+        this.loadAllTreeHorses();
+        this.loadTree();
+      },
+      error => {
+        alert('Could not delete horse ' + name + '.');
+        this.defaultServiceErrorHandling(error);
+      }
+    );
+  }
+
+  /**
+   * goes to details page of specified id
+   *
+   * @param id of the horse who´s details are to be shown
+   */
+  goToDetails(id: number) {
+    this.router.navigate(['/details-horse/' + id]);
   }
 
 
@@ -119,8 +157,32 @@ export class BloodlineComponent implements OnInit {
     element.style.marginLeft = offset.toString() + 'px';
     element.className = 'form-control';
     element.id = currHorse.id.toString();
+    element.style.alignContent='center';
+    element.style.alignItems = 'center';
+    element.style.marginBottom='5px';
 
+    //details button
+    const detBtn = document.createElement('button');
+    detBtn.type = 'button';
+    //detBtn.className = 'btn btn-light btn-outline-secondary';
+    detBtn.style.alignContent = 'center';
+    detBtn.textContent = 'DETAILS';
+    detBtn.addEventListener('click', (e) => this.goToDetails(currHorse.id));
 
+    //detBtn.appendChild(routeLink);
+    console.log(detBtn);
+    console.log(element);
+    element.appendChild(detBtn);
+
+    //delete button
+    const delBtn = document.createElement('button');
+    delBtn.style.margin = '5px';
+    delBtn.type = 'button';
+    //delBtn.className = 'btn btn-outline-secondary';
+    delBtn.textContent = 'DELETE';
+    delBtn.addEventListener('click', (e) => this.deleteHorse(currHorse.id, currHorse.name));
+
+    element.appendChild(delBtn);
     //make children expandable
     /*if (level === 0) {
       element.hidden = false;
@@ -142,4 +204,6 @@ export class BloodlineComponent implements OnInit {
       this.createFieldsRec(parent2, level + 1, horsesDecimated);
     }
   }
+
+
 }
